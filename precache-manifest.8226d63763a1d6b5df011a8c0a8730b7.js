3f2f6cd71467ae967d006c43b787e667b279626a
“naysamoreno” <naysaymoreno@gmail.com>
2020-04-04 16:02:38 -0500

    Updates

---
+++ precache-manifest.8226d63763a1d6b5df011a8c0a8730b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec8992c20fe6066d9a481e4a77ff31f",
+    "url": "/goobydooby/index.html"
+  },
+  {
+    "revision": "186f8c01117ae81f8f70",
+    "url": "/goobydooby/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d4514ce3602bbdcad12",
+    "url": "/goobydooby/static/js/2.0262cee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goobydooby/static/js/2.0262cee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186f8c01117ae81f8f70",
+    "url": "/goobydooby/static/js/main.511ed6ab.chunk.js"
+  },
+  {
+    "revision": "3434a16b8782417d8fe2",
+    "url": "/goobydooby/static/js/runtime-main.9837c156.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goobydooby/static/media/logo.5d5d9eef.svg"
+  }
+]);
